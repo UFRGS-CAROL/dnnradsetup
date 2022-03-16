2947f21bb1b7ed9ea0c74de16c2ee9a1ded03af8
prbodmann <prbodmann@gmail.com>
2022-03-16 13:37:34 -0300

    changed tensorflow_dnn.py

--- tensorflow_dnns.py
+++ tensorflow_dnns.py
@@ -1,27 +1,53 @@
 #!/usr/bin/python3.8
 
+
+
 import os
 import tensorflow
 import console_logger
 from common_tf_and_pt import DNNType, INCEPTION_V3, RESNET_50, SSD_MOBILENET_V2, EFFICIENT_DET_LITE3, BATCH_SIZE_GPU
 from common_tf_and_pt import INCEPTION_B7, RETINA_NET_RESNET_FPN50, FASTER_RCNN_RESNET_FPN50
-from common_tf_and_pt import parse_args, Timer
+from common_tf_and_pt import parse_args, Timer, load_image_list
+from tensorflow.keras.applications import ResNet50, InceptionV3, efficientnet
+from tensorflow.keras.applications import resnet50.preprocess_input, inception_v3.preprocess_input
+from tensorflow.keras.applications.efficientnet import EfficientNetB7
+import numpy as np
+
+
+def load_dataset(transforms, image_list_path: str, logger: logging.Logger,
+                 batch_size: int, device: str) -> torch.tensor:
+    timer = Timer()
+    timer.tic()
+    images = load_image_list(image_list_path)
+    resized_images = list()
+    for image in images:
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = transforms(x)
+        resized_images.append(x)
+    timer.toc()
+    logger.debug(f"Input images loaded and resized successfully: {timer}")
+    return input_tensor
+
+
+
 
 DNN_MODELS = {
     INCEPTION_V3: {
-        "model": None,
+        "model": InceptionV3,
         "type": DNNType.CLASSIFICATION,
-        "transform": None
+        "transform": inception_v3.preprocess_input
     },
     RESNET_50: {
-        "model": None,
+        "model": ResNet50,
         "type": DNNType.CLASSIFICATION,
-        "transform": None
+        "transform": resnet50.preprocess_input
     },
     INCEPTION_B7: {
         "model": None,
         "type": DNNType.CLASSIFICATION,
-        "transform": None
+        "transform": none
     },
     # Object detection, segmentation, and keypoint
     SSD_MOBILENET_V2: {
@@ -68,7 +94,52 @@ def main():
     gold_path = args.goldpath
     precision = args.precision
     model_name = args.model
+    model_parameters=DNN_MODELS[model_name]
+    model=model_parameters["model"](weights='imagenet')
+    dnn_type = model_parameters["type"]
+    transform = model_parameters["transform"]
+    input_list=load_dataset(transforms=transform, image_list_path=image_list_path,
+                              logger=output_logger, batch_size=batch_size, device=device)
+    # Main setup loop
+    setup_iteration = 0
+    while setup_iteration < iterations:
+        total_errors = 0
+        # Loop over the input list
+        for batch_iteration, batched_input in enumerate(input_list):
+            timer.tic()
+            dnn_log_helper.start_iteration()
+            current_output = model(batched_input)
+            dnn_log_helper.end_iteration()
+            timer.toc()
+            iteration_out = f"It:{setup_iteration} - input it:{batch_iteration}"
+            iteration_out += f" inference time:{timer}"
 
+            # Then compare the golden with the output
+            timer.tic()
+            errors = 0
+            if generate is False:
+                current_gold = dnn_gold_tensors[batch_iteration]
+                errors = compare_output_with_gold(dnn_output_tensor=current_output, dnn_golden_tensor=current_gold,
+                                                  dnn_type=dnn_type, batch_size=batch_size,
+                                                  setup_iteration=setup_iteration, batch_iteration=batch_iteration)
+            else:
+                assert len(current_output) == batch_size, str(current_output)
+                dnn_gold_tensors.append(current_output)
+
+            total_errors += errors
+            timer.toc()
+            iteration_out += f", gold compare time:{timer} errors:{errors}"
+            output_logger.debug(iteration_out)
+
+        # Reload after error
+        if total_errors != 0:
+            del input_list
+            del model
+            model = model=model_parameters["model"](weights='imagenet')
+            input_list = load_dataset(transforms=transform, image_list_path=image_list_path,
+                                      logger=output_logger, batch_size=batch_size, device=device)
+
+        setup_iteration += 1
     with tensorflow.device(device_name=device):
         pass
 
